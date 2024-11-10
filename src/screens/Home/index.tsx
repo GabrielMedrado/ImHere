ab144d9778a1e78f6a2e0f80a8e8376cb7fcd5ed
Gabriel Medrado <medradoogabriel@gmail.com>
2024-11-10 01:21:24 -0300

    feat: create form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,12 @@
-import { Text, View, TextInput } from "react-native"
+import { Text, View, TextInput, TouchableOpacity} from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar participante");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -12,11 +17,18 @@ export function Home() {
         Sábado, 09 de Novembro de 2024
       </Text>
 
+    <View style={styles.form}>
       <TextInput 
         style={styles.input}
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
+     </View>
     </View>
   )
 }
